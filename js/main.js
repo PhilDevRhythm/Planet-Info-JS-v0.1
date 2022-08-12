12ed1b5cfb1ae4cd16e6870081480e23950b8c55
PhilDevRhythm <philippe.doussoulin@gmail.com>
2022-08-11 21:46:14 -0400

    1era clase

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var myArray = ["What is the meaning of life?", 56, true];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+
+
+
+//clases 1
+
+let age = 36;
+let nameFirst = "Phil";
+const YEAR = 2022;
+let birthDate = YEAR - age;
+
+console.log(typeof(nameFirst));
+
+
+
+
+
