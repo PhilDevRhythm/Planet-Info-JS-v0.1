e31104d375bf35d080bfe93661948a5dea620761
Phil <phildevrhythm@gmail.com>
2022-09-20 10:51:33 -0300

    added Planetinfo v2

--- js/planetInfo.js
+++ js/planetInfo.js
@@ -6,31 +6,29 @@ function showPlanetInfo(planet) {
     let planetLife = tempInfo.vida
     let planetDistance = tempInfo.distance
     let planetImage = tempInfo.imageLink
-    // console.log(tempInfo);
-    // console.log(planetName);
-    // console.log(planetSats);
-    // console.log(planetDistance);
-    // console.log(planetLife);
-    // console.log(planetImage);
     let imageTemp = document.getElementById("showImage")
     imageTemp.innerHTML = '<img src=' + planetImage + '></img>'
     let textColTemp = document.getElementById("showData1")
     textColTemp.innerHTML = '<p> El planeta ' + planetName  +  'tiene conocidos hasta el dia de hoy ' + planetSats + ' lunas o satelites naturales</p>' 
     let textColTemp2 = document.getElementById("showData2")
     textColTemp2.innerHTML = planetName + '<p> esta a ' + planetDistance * 150 + 'millones de kilometros del planeta Tierra</p>'
-    
+    if (planetLife == true){
+        textColTemp2.innerHTML = '<p> En el planeta' + planetName + 'hay seres vivos</p>'
+    }else{
+        textColTemp2.innerHTML = '<p> En el planeta' + planetName + 'no hay vida conocida</p>'
+    }
     return
 }
  
 const SolarSystem = [
-    { id: "SS1", nombre: "Mercurio", Sats: 0, vida: "NO", distance: 0.39, imageLink: "../images/mercurio.jpg"},
-    { id: "SS2", nombre: "Venus", Sats: 0, vida: "NO", distance: 0.72, imageLink: "../images/mercurio.jpg"},
-    { id: "SS3", nombre: "Tierra", Sats: 1, vida: "SI", distance: 1, imageLink: "../images/mercurio.jpg"},
-    { id: "SS4", nombre: "Marte", Sats: 2, vida: "NO", distance: 1.52, imageLink: "../images/mercurio.jpg"},
-    { id: "SS5", nombre: "Jupiter", Sats: 79, vida: "NO", distance: 5.20, imageLink: "../images/mercurio.jpg"},
-    { id: "SS6", nombre: "Saturno", Sats: 82, vida: "NO", distance: 9.58, imageLink: "../images/mercurio.jpg"},
-    { id: "SS7", nombre: "Urano", Sats: 27, vida: "NO", distance: 19.23, imageLink: "../images/mercurio.jpg"},
-    { id: "SS8", nombre: "Neptuno", Sats: 14, vida: "NO", distance: 30.1, imageLink: "../images/neptuno.jpg"}
+    { id: "SS1", nombre: "Mercurio", Sats: 0, vida: false, distance: 0.39, imageLink: "../images/mercurio.jpg"},
+    { id: "SS2", nombre: "Venus", Sats: 0, vida: false, distance: 0.72, imageLink: "../images/venus.webp"},
+    { id: "SS3", nombre: "Tierra", Sats: 1, vida: true, distance: 1, imageLink: "../images/mercurio.jpg"},
+    { id: "SS4", nombre: "Marte", Sats: 2, vida: false, distance: 1.52, imageLink: "../images/mars.png"},
+    { id: "SS5", nombre: "Jupiter", Sats: 79, vida: false, distance: 5.20, imageLink: "../images/mercurio.jpg"},
+    { id: "SS6", nombre: "Saturno", Sats: 82, vida: false, distance: 9.58, imageLink: "../images/mercurio.jpg"},
+    { id: "SS7", nombre: "Urano", Sats: 27, vida: false, distance: 19.23, imageLink: "../images/mercurio.jpg"},
+    { id: "SS8", nombre: "Neptuno", Sats: 14, vida: false, distance: 30.1, imageLink: "../images/neptuno.jpg"}
 ]
 
 //funciones para eleccion 
